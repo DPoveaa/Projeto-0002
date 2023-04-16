7b921b86817e0a392f7ea138a03873acd71b6ab6
Giulia Fernandes <giulia.abreu2004@hotmail.com>
2023-04-16 17:58:54 -0300

    Player 0.1

---
+++ Assets/Player/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D playerRB;
+    private float movePlayer;
+
+    public float jumpforce;
+    public bool pulo, isgrounded;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        movePlayer = Input.GetAxis("Horizontal");
+        playerRB.velocity = new Vector2(movePlayer * speed, playerRB.velocity.y);
+        pulo = Input.GetButtonDown("Jump");
+        if (pulo == true && isgrounded == true) {
+            playerRB.AddForce(new Vector2(0, jumpforce));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("ground")) {
+            isgrounded = true;
+        
+        }
+    }
+}
+
