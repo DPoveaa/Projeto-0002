62db24da69f0957d99c303108e720ad044a5bffd
Povea <danieldpcaires@gmail.com>
2023-04-17 17:20:51 -0300

    Enemy v0.03
    Arrumado alguns erros.
    Inserido hitbox para matar o player.
    Código enfeitado.

--- Assets/Enemy/Script/Enemy.cs
+++ Assets/Enemy/Script/Enemy.cs
@@ -5,38 +5,39 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    #region Vars
     public float speed = 3f;
     public GameObject leftLimit;
     public GameObject rightLimit;
-    public GameObject sensor;
+    public GameObject killCheck;
+    public GameObject GroundCheck;
     private bool isGrounded = true;
     public bool movingRight = true;
-
     private Rigidbody2D rb;
-
+    #endregion
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); //Will get the enemy rigidbody so the script can manipulate
     }
 
     void FixedUpdate()
     {
-
+        #region Movement
         rb.velocity = new Vector2(speed, rb.velocity.y);
-
-        if (isGrounded)
-        {
-            Debug.Log(isGrounded + "if");
-        }
+        #endregion
     }
-    void Flip()
+
+    #region Flip
+    void Flip() // This will turn the enemy sprite and sensors to the other side
     {
         Vector3 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
         speed = speed * -1;
     }
+    #endregion
 
+    #region OnTriggerExit
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
@@ -45,15 +46,24 @@ public class Enemy : MonoBehaviour
             isGrounded = false;
         }
     }
+    #endregion
+
+    #region OntriggerEnter
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
+        else if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
     }
+    #endregion
 
-        void OnCollisionEnter2D(Collision2D col)
+    #region OnCollisionEnter
+    void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
@@ -61,4 +71,5 @@ public class Enemy : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    #endregion
 }
